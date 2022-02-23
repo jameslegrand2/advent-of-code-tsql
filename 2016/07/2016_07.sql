e3e54ae7d633d6330f7763586919d71f88e0f893
jameslegrand2 <jameslegrand2@gmail.com>
2022-02-23 17:16:35 -0600

    2016 day 7

--- 2016/07/2016_07.sql
+++ 2016/07/2016_07.sql
@@ -20,19 +20,24 @@ DROP TABLE IF EXISTS #Input
 DROP TABLE IF EXISTS #Tracking
 DROP TABLE IF EXISTS #Numbers
 DROP TABLE IF EXISTS #SubText
+DROP TABLE IF EXISTS #aba
+DROP TABLE IF EXISTS #bab
 
 --Load the file containing the input data
 CREATE TABLE #Input(IPAddress varchar(max))
 SET @SQL = 'BULK INSERT #Input FROM ''' + @InputFile + ''''
 EXEC(@SQL);
 
-CREATE TABLE #Tracking (TextId INT IDENTITY, OriginalText VARCHAR(MAX),BaseHasABBA BIT,HypersetHasABBA BIT)
+--Create table to track results for each input line
+CREATE TABLE #Tracking (TextId INT IDENTITY, OriginalText VARCHAR(MAX),BaseHasABBA BIT,HypersetHasABBA BIT,SupportsSSL BIT)
 INSERT INTO #Tracking
     SELECT IPAddress
         ,0
         ,0
+        ,0
     FROM #Input
 
+--Create table to have a list of numbers for joining on
 CREATE TABLE #Numbers (num INT);
 WITH
     n1(n) AS (SELECT 1 UNION ALL SELECT 1),    -- returns 1 row
@@ -43,12 +48,18 @@ WITH
 INSERT INTO #numbers
 SELECT n FROM nums;
 
+--Loop through the lines
 SET @TotalText = (SELECT COUNT(TextId) FROM #Tracking)
-
 WHILE @CurrentTextId <= @TotalText
 BEGIN
+    --Create table to track sections of the line
     CREATE TABLE #SubText (SubtextId INT IDENTITY,SubText VARCHAR(MAX),SubtextLength INT);
 
+    --Create tables to track ABA and BAB
+    CREATE TABLE #aba (a varchar(1),b varchar(1))
+    CREATE TABLE #bab (a varchar(1),b varchar(1));
+
+    --Check the first non-hyperset section for ABBA
     WITH SearchText1 AS (
         SELECT [value] SearchText1
             , LEN([value]) Text1Length
@@ -69,28 +80,49 @@ BEGIN
     )
     UPDATE #Tracking
     SET BaseHasABBA = (SELECT Result FROM Search1)
-    WHERE TextId = @CurrentTextId
+    WHERE TextId = @CurrentTextId;
 
+    --Check the first non-hyperset section for ABA
+    WITH SearchText1 AS (
+        SELECT [value] SearchText1
+            , LEN([value]) Text1Length
+        FROM string_split((SELECT OriginalText FROM #Tracking WHERE TextId = @CurrentTextId),'[')
+        WHERE [value] NOT LIKE '%]%'
+    )
+    INSERT INTO #ABA
+    SELECT SUBSTRING(SearchText1,num,1)
+        , SUBSTRING(SearchText1,num+1,1)
+    FROM SearchText1 t
+        CROSS JOIN #Numbers n
+    WHERE num <= Text1Length - 2
+        AND SUBSTRING(SearchText1,num,3) = REVERSE(SUBSTRING(SearchText1,num,3))
+        AND SUBSTRING(SearchText1,num,1) != SUBSTRING(SearchText1,num+1,1)
+
+    --Further split up the sections of the line
     INSERT INTO #SubText
         SELECT [value]
         , LEN([value])
         FROM string_split((SELECT OriginalText FROM #Tracking WHERE TextId = @CurrentTextId),'[')
         WHERE [value] LIKE '%]%'
 
+    --Loop through the remaining pairs of hyperset and non-hyperset sections
     SET @CurrentSubTextId = 1
     SET @TotalSubText = (SELECT COUNT(SubtextId) FROM #SubText)
-
     WHILE @CurrentSubTextId <= @TotalSubText
     BEGIN
+        --Get the hyperset section
         SELECT TOP 1 @Hyperset = [value]
         FROM string_split((SELECT Subtext FROM #SubText WHERE SubtextId = @CurrentSubtextId),']')
 
+        --Get the non-hyperset section
         SELECT @NotHyperset = [value]
         FROM string_split((SELECT Subtext FROM #SubText WHERE SubtextId = @CurrentSubtextId),']')
         WHERE [value] <> @Hyperset;
 
+        --Skip checking the hyperset section if an ABBA one has already been found
         IF (SELECT HypersetHasABBA FROM #Tracking WHERE TextId = @CurrentTextId) = 0
         BEGIN
+            --Check the hyperset section for ABBA
             WITH SearchText2 AS (
                 SELECT @Hyperset SearchText2
                     ,LEN(@Hyperset) Text2Length
@@ -109,10 +141,26 @@ BEGIN
             UPDATE #Tracking
                 SET HypersetHasABBA = (SELECT Result FROM Search2)
                 WHERE TextId = @CurrentTextId
-        END
+        END;
 
+        --Check the hyperset section for ABA
+        WITH SearchText2 AS (
+            SELECT @Hyperset SearchText2
+                ,LEN(@Hyperset) Text2Length
+        )
+        INSERT INTO #BAB
+        SELECT SUBSTRING(SearchText2,num+1,1)
+            , SUBSTRING(SearchText2,num,1)
+        FROM SearchText2 t
+            CROSS JOIN #Numbers n
+        WHERE num <= Text2Length - 2
+            AND SUBSTRING(SearchText2,num,3) = REVERSE(SUBSTRING(SearchText2,num,3))
+            AND SUBSTRING(SearchText2,num,1) != SUBSTRING(SearchText2,num+1,1)
+
+        --Skip checking the non-hyperset section if an ABBA one has already been found
         IF (SELECT BaseHasABBA FROM #Tracking WHERE TextId = @CurrentTextId) = 0
         BEGIN
+            --Check the non-hyperset section for ABBA
             WITH SearchText1 AS (
                 SELECT @NotHyperset SearchText1
                     ,LEN(@NotHyperset) Text1Length
@@ -132,19 +180,56 @@ BEGIN
             UPDATE #Tracking
             SET BaseHasABBA = (SELECT Result FROM Search1)
             WHERE TextId = @CurrentTextId
-        END
+        END;
 
+        --Check the non-hyperset section for ABA
+        WITH SearchText1 AS (
+            SELECT @NotHyperset SearchText1
+                ,LEN(@NotHyperset) Text1Length
+        )
+        INSERT INTO #ABA
+        SELECT SUBSTRING(SearchText1,num,1)
+            , SUBSTRING(SearchText1,num+1,1)
+        FROM SearchText1 t
+            CROSS JOIN #Numbers n
+        WHERE num <= Text1Length - 2
+            AND SUBSTRING(SearchText1,num,3) = REVERSE(SUBSTRING(SearchText1,num,3))
+            AND SUBSTRING(SearchText1,num,1) != SUBSTRING(SearchText1,num+1,1)
+
+        --Iterate to the next pair
         SET @CurrentSubTextId += 1
     END
 
+    --Calculate if there are any aba/bab pairs
+    UPDATE #Tracking
+    SET SupportsSSL = 
+        CASE
+            WHEN (
+                SELECT COUNT(a.a)
+                FROM #aba a
+                    CROSS JOIN #bab b
+                WHERE a.a = b.a
+                    AND a.b = b.b) > 0 THEN 1
+            ELSE 0
+        END
+    WHERE TextId = @CurrentTextId
+
+    --Cleanup section testing, iterate to the next line
     DROP TABLE #SubText
+    DROP TABLE #aba
+    DROP TABLE #bab
     SET @CurrentTextId += 1
 END
 
+--Return results
 SELECT COUNT(TextId) 
 FROM #Tracking
 WHERE BaseHasABBA = 1
     AND HypersetHasABBA <> 1
+    
+SELECT COUNT(TextId) 
+FROM #Tracking
+WHERE SupportsSSL = 1
 
 --Cleanup
 DROP TABLE #Input
