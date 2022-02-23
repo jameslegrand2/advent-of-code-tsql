323d3b5a91dcb3221c8cf42fc36f3e2bf7f3833c
jameslegrand2 <jameslegrand2@gmail.com>
2022-02-23 06:07:05 -0600

    2016 day 7 part 1

---
+++ 2016/07/2016_07.sql
@@ -0,0 +1,152 @@
+--Set to the repository location on the local machine
+DECLARE @RepositoryLocation VARCHAR(MAX) = 'C:\Users\Jim LeGrand\Documents\T-SQL\advent-of-code-tsql\advent-of-code-tsql'
+
+--Speed up execution
+SET NOCOUNT ON;
+SET ANSI_WARNINGS OFF;
+
+--Variable declarations
+DECLARE @InputFile VARCHAR(MAX) = @RepositoryLocation + '\2016\07\Input.txt'
+DECLARE @SQL VARCHAR(MAX)
+DECLARE @CurrentTextId INT = 1
+DECLARE @TotalText INT
+DECLARE @CurrentSubTextId INT
+DECLARE @TotalSubText INT
+DECLARE @Hyperset VARCHAR(MAX)
+DECLARE @NotHyperset VARCHAR(MAX)
+
+--Cleanup in case of previous errors
+DROP TABLE IF EXISTS #Input
+DROP TABLE IF EXISTS #Tracking
+DROP TABLE IF EXISTS #Numbers
+DROP TABLE IF EXISTS #SubText
+
+--Load the file containing the input data
+CREATE TABLE #Input(IPAddress varchar(max))
+SET @SQL = 'BULK INSERT #Input FROM ''' + @InputFile + ''''
+EXEC(@SQL);
+
+CREATE TABLE #Tracking (TextId INT IDENTITY, OriginalText VARCHAR(MAX),BaseHasABBA BIT,HypersetHasABBA BIT)
+INSERT INTO #Tracking
+    SELECT IPAddress
+        ,0
+        ,0
+    FROM #Input
+
+CREATE TABLE #Numbers (num INT);
+WITH
+    n1(n) AS (SELECT 1 UNION ALL SELECT 1),    -- returns 1 row
+    n2(n) AS (SELECT 1 FROM n1 AS x, n1 AS y), -- 4 rows
+    n3(n) AS (SELECT 1 FROM n2 AS x, n2 AS y), -- 16
+    n4(n) AS (SELECT 1 FROM n3 AS x, n3 AS y), -- 256
+    nums(n) AS (SELECT ROW_NUMBER() OVER(ORDER BY n) FROM n4)
+INSERT INTO #numbers
+SELECT n FROM nums;
+
+SET @TotalText = (SELECT COUNT(TextId) FROM #Tracking)
+
+WHILE @CurrentTextId <= @TotalText
+BEGIN
+    CREATE TABLE #SubText (SubtextId INT IDENTITY,SubText VARCHAR(MAX),SubtextLength INT);
+
+    WITH SearchText1 AS (
+        SELECT [value] SearchText1
+            , LEN([value]) Text1Length
+        FROM string_split((SELECT OriginalText FROM #Tracking WHERE TextId = @CurrentTextId),'[')
+        WHERE [value] NOT LIKE '%]%'
+    ), Search1 AS (
+        SELECT 
+            MAX(CASE 
+                WHEN
+                    SUBSTRING(SearchText1,num,4) = REVERSE(SUBSTRING(SearchText1,num,4))
+                    AND SUBSTRING(SearchText1,num,1) != SUBSTRING(SearchText1,num+1,1)
+                    THEN 1
+                ELSE 0
+            END) Result
+        FROM SearchText1 t
+            CROSS JOIN #Numbers n
+        WHERE num <= Text1Length - 3
+    )
+    UPDATE #Tracking
+    SET BaseHasABBA = (SELECT Result FROM Search1)
+    WHERE TextId = @CurrentTextId
+
+    INSERT INTO #SubText
+        SELECT [value]
+        , LEN([value])
+        FROM string_split((SELECT OriginalText FROM #Tracking WHERE TextId = @CurrentTextId),'[')
+        WHERE [value] LIKE '%]%'
+
+    SET @CurrentSubTextId = 1
+    SET @TotalSubText = (SELECT COUNT(SubtextId) FROM #SubText)
+
+    WHILE @CurrentSubTextId <= @TotalSubText
+    BEGIN
+        SELECT TOP 1 @Hyperset = [value]
+        FROM string_split((SELECT Subtext FROM #SubText WHERE SubtextId = @CurrentSubtextId),']')
+
+        SELECT @NotHyperset = [value]
+        FROM string_split((SELECT Subtext FROM #SubText WHERE SubtextId = @CurrentSubtextId),']')
+        WHERE [value] <> @Hyperset;
+
+        IF (SELECT HypersetHasABBA FROM #Tracking WHERE TextId = @CurrentTextId) = 0
+        BEGIN
+            WITH SearchText2 AS (
+                SELECT @Hyperset SearchText2
+                    ,LEN(@Hyperset) Text2Length
+            ), Search2 AS (
+                SELECT 
+                    MAX(CASE
+                        WHEN SUBSTRING(SearchText2,num,4) = REVERSE(SUBSTRING(SearchText2,num,4))
+                            AND SUBSTRING(SearchText2,num,1) != SUBSTRING(SearchText2,num+1,1)
+                            THEN 1
+                        ELSE 0 END
+                    ) Result
+                FROM SearchText2 t
+                    CROSS JOIN #Numbers n
+                WHERE num <= Text2Length - 3
+            )
+            UPDATE #Tracking
+                SET HypersetHasABBA = (SELECT Result FROM Search2)
+                WHERE TextId = @CurrentTextId
+        END
+
+        IF (SELECT BaseHasABBA FROM #Tracking WHERE TextId = @CurrentTextId) = 0
+        BEGIN
+            WITH SearchText1 AS (
+                SELECT @NotHyperset SearchText1
+                    ,LEN(@NotHyperset) Text1Length
+            ), Search1 AS (
+                SELECT 
+                    MAX(CASE 
+                        WHEN
+                            SUBSTRING(SearchText1,num,4) = REVERSE(SUBSTRING(SearchText1,num,4))
+                            AND SUBSTRING(SearchText1,num,1) != SUBSTRING(SearchText1,num+1,1)
+                            THEN 1
+                        ELSE 0
+                    END) Result
+                FROM SearchText1 t
+                    CROSS JOIN #Numbers n
+                WHERE num <= Text1Length - 3
+            )
+            UPDATE #Tracking
+            SET BaseHasABBA = (SELECT Result FROM Search1)
+            WHERE TextId = @CurrentTextId
+        END
+
+        SET @CurrentSubTextId += 1
+    END
+
+    DROP TABLE #SubText
+    SET @CurrentTextId += 1
+END
+
+SELECT COUNT(TextId) 
+FROM #Tracking
+WHERE BaseHasABBA = 1
+    AND HypersetHasABBA <> 1
+
+--Cleanup
+DROP TABLE #Input
+DROP TABLE #Tracking
+DROP TABLE #Numbers
